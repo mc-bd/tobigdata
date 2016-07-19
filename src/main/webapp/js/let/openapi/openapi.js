f8d84753302afa754cdda899098012d06d29e5c4
t1t1 <cha@hanmail2.net>
2016-07-19 15:12:14 +0900

    js; function -> json; 외부에서 호출

--- src/main/webapp/js/let/openapi/openapi.js
+++ src/main/webapp/js/let/openapi/openapi.js
@@ -4,116 +4,126 @@
 (function() {
 	
 	$(document).ready(function() {
-		bindEvent();
+		openapi.bindEvent();
 	});
 	
-	// =======================================================================
-	// bind; event
-	// =======================================================================
-	
-	function bindEvent() {
-		$('#add').on('click', function() {
-			__.popup('/openapi/create', {
-				height: 400
+	var openapi = {
+		// =======================================================================
+		// bind; event
+		// =======================================================================
+		
+		bindEvent: function() {
+			$('#add').on('click', function() {
+				__.popup('/openapi/create', {
+					height: 400
+				});
 			});
-		});
-		$('#save').on('click', openapiCreatePost);
-		$('#close').on('click', function() {
-			__.close();
-		});
-	};
-	
-	// =======================================================================
-	// To server side;
-	// =======================================================================
-	
-	function openapiGet() {
-		location.href = '/openapi';
-	};
-	function openapiPostList() {
-		var _pageIndex = 1;
-		__.ajax({
-			url: '/openapi',
-			dataType: 'html',
-			method: 'POST',
-			data: {
-				serviceId: $('#serviceId').val(),
-				serviceName: $('#serviceName').val(),
-				managerName: $('#managerName').val(),
-				serviceIp: $('#serviceIp').val(),
-				pageIndex: _pageIndex
-			},
-			success: function(data) {
-//				$('.content-table').html(data);
-			}
-		});
-	} ;
-	function openapiGetDetailByPost() {
-		// 
-		__.ajax({
-			url: '/openapi',
-			method: 'POST',
-			data: {
-				"serviceId": "11",
-			},
-			success: function(data) {
-				alert(JSON.stringify(data));
-			}
-		});
-	};
-	function openapiCreateGet() {
-	};
-	function openapiCreatePost() {
-		//
-		__.ajax({
-			url: '/openapi/create',
-			method: 'POST',
-			data: {
-				serviceId: $('#serviceId').val(),
-				serviceName: $('#serviceName').val(),
-				managerName: $('#managerName').val(),
-				managerTel: $('#managerTel').val(),
-				managerEmail: $('#managerEmail').val(),
-				serviceIp: $('#serviceIp').val(),
-				permissionKey: $('#permissionKey').val(),				
-			},
-			success: function(data) {
-				__.alert('저장되었습니다.');
+			$('#save').on('click', this.openapiCreatePost);
+			$('#close').on('click', function() {
 				__.close();
-			}
-		});
-	};
-	function openapiPut() {
-		//
-		__.ajax({
-			url: '/openapi',
-			method: 'PUT',
-			data: {
-				"serviceId": "11",
-				"serviceName": "2",
-				"managerName": "33",
-				"managerTel": "44",
-				"managerEmail": "55",
-				"serviceIp": "66",
-				"permissionKey": "77"
-			},
-			success: function(data) {
-				alert(JSON.stringify(data));
-			}
-		});
-	};
-	function openapiDelete() {
-		//
-		__.ajax({
-			url: '/openapi',
-			method: 'DELETE',
-			data: {
-				"serviceId": "11",
-			},
-			success: function(data) {
-				alert(JSON.stringify(data));
-			}
-		});
-	};
+			});
+		},
+		
+		// =======================================================================
+		// To server side;
+		// =======================================================================
+		
+		openapiGet: function() {
+			location.href = '/openapi';
+		},
+		openapiPostList: function() {
+			
+			//
+			$('content-table').find('table').find('tr').filter(':not(:nth-child(1))').remove();
+			
+			var _pageIndex = 1;
+			__.ajax({
+				url: '/openapi',
+				dataType: 'html',
+				method: 'POST',
+				data: {
+					serviceId: $('#serviceId').val(),
+					serviceName: $('#serviceName').val(),
+					managerName: $('#managerName').val(),
+					serviceIp: $('#serviceIp').val(),
+					pageIndex: _pageIndex
+				},
+				success: function(data) {
+//				$('.content-table').html(data);
+				}
+			});
+		},
+		openapiGetDetailByPost: function() {
+			// 
+			__.ajax({
+				url: '/openapi',
+				method: 'POST',
+				data: {
+					"serviceId": "11",
+				},
+				success: function(data) {
+					alert(JSON.stringify(data));
+				}
+			});
+		},
+		openapiCreateGet: function() {
+		},
+		openapiCreatePost: function() {
+			//
+			__.ajax({
+				url: '/openapi/create',
+				method: 'POST',
+				data: {
+					serviceId: $('#serviceId').val(),
+					serviceName: $('#serviceName').val(),
+					managerName: $('#managerName').val(),
+					managerTel: $('#managerTel').val(),
+					managerEmail: $('#managerEmail').val(),
+					serviceIp: $('#serviceIp').val(),
+					permissionKey: $('#permissionKey').val(),				
+				},
+				success: function(data) {
+					__.alert('저장되었습니다.');
+					__.close();
+					openapiPostList();
+				}
+			});
+		},
+		openapiPut: function() {
+			//
+			__.ajax({
+				url: '/openapi',
+				method: 'PUT',
+				data: {
+					"serviceId": "11",
+					"serviceName": "2",
+					"managerName": "33",
+					"managerTel": "44",
+					"managerEmail": "55",
+					"serviceIp": "66",
+					"permissionKey": "77"
+				},
+				success: function(data) {
+					alert(JSON.stringify(data));
+				}
+			});
+		},
+		openapiDelete: function() {
+			//
+			__.ajax({
+				url: '/openapi',
+				method: 'DELETE',
+				data: {
+					"serviceId": "11",
+				},
+				success: function(data) {
+					alert(JSON.stringify(data));
+				}
+			});
+		}
+		
+	}
+	
+	window.openapi = openapi;
 	
 })();
