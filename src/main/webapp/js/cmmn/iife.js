1008b39706ac174433dc1a0496cc68743e4a6e34
t1t1 <cha@hanmail2.net>
2016-07-22 16:40:30 +0900

    <spring:message ... 삭제; in *.js

--- src/main/webapp/js/cmmn/iife.js
+++ src/main/webapp/js/cmmn/iife.js
@@ -7,7 +7,7 @@
 		
 		// 로그아웃 
 		$('body').on('click', 'button.logout', function() {
-			location.href = '<spring:message code="url.logout" />';
+			location.href = '/login/logout';
 		});
 		
 		// queryString -> set input value
